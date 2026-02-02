be1747f57a5a28dae6183d0f89974fdeea825e9e
professorwade <121523833+professorwade@users.noreply.github.com>
2026-02-02 13:45:48 -0500

    added string and scope examples

---
+++ week4/scope.py
@@ -0,0 +1,10 @@
+def some_function():
+    x = 5
+    print(x)
+
+print(x) # This will raise a NameError because x is not defined in this scope
+
+
+x = 10
+some_function()
+print(x) # This will print 10 because x is defined in the global scope  
--- week4/strings.py
+++ week4/strings.py
@@ -1,3 +1,15 @@
+"""Week 4: Strings
+Strings are sequences of characters.
+You can use single quotes ' or double quotes " to create strings.
+You can use triple quotes ''' or \""" for multi-line strings.
+
+Note that triple quotes can also be used for multi-line comments.
+triple quotes can also contain single and double quotes without needing to escape them.
+
+Triple quotes are often used for docstrings, which are multi-line comments that describe functions, classes, or modules.
+
+"""
+
 a_string = "Hello my name is Wade."
 print(len(a_string))
 
@@ -15,6 +27,21 @@ print(r"C:\users\new_folder")
 sentence = "The quick brown fox"
 print("fox" in sentence)    # Output: True
 print("cat" in sentence)    # Output: False
+sentence = sentence.upper()
+print(sentence)             # Output: THE QUICK BROWN FOX
 
 print(ord("A")) # Output: 65
 print(chr(128640)) # Output: 🚀
+
+# strings are made up of characters, which are represented by numbers in the computer
+# consequently if I iterate over a string, I get each character one at a time
+for char in "Hello":
+    print(char)
+
+# strings are compared lexicographically using the ASCII values of their characters    
+print("A" < "B") # Output: True
+print("apple" < "banana") # Output: True
+print("apple" < "Apple") # Output: False, because lowercase letters have higher ASCII values than uppercase letters
+print("apple" < "apples") # Output: True, because "apple" is shorter than "apples"
+
+
