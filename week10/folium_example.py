578d10bd52c4c75f8c35aa7a3addf6562ed12e25
professorwade <schofieldw@centralvirginia.edu>
2026-03-13 17:18:03 -0400

    Added folium mapping example

---
+++ week10/folium_example.py
@@ -0,0 +1,8 @@
+import folium
+
+# Define the starting coordinates (e.g., the center of Boston, MA)
+# The default basemap is OpenStreetMap
+m = folium.Map(location=[42.3601, -71.0589], zoom_start=12)
+
+# Save the map as an HTML file
+m.save("boston_map.html")
