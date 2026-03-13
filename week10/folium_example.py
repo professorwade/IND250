1993a961d8029e5ed09132c967cba48ac5e96ad8
professorwade <schofieldw@centralvirginia.edu>
2026-03-13 17:35:33 -0400

    Added URL to Folium

--- week10/folium_example.py
+++ week10/folium_example.py
@@ -1,5 +1,5 @@
 import folium
-
+# https://python-visualization.github.io/folium/latest/
 # Define the starting coordinates (e.g., the center of Boston, MA)
 # The default basemap is OpenStreetMap
 m = folium.Map(location=[42.3601, -71.0589], zoom_start=12)
